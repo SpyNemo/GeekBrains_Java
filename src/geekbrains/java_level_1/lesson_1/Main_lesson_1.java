f64ad1bdd21cf44a578dd3f5e1dd4a7a2c47fdb9
IvanTheT <ivan.titarchuk@gmail.com>
2017-10-22 22:58:37 -0500

    Lesson_1 Final

--- src/geekbrains/java_level_1/lesson_1/Main_lesson_1.java
+++ src/geekbrains/java_level_1/lesson_1/Main_lesson_1.java
@@ -3,21 +3,38 @@ import java.util.Scanner;
 
 //1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
 
-
-//4. Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит в
-//    пределах 10 до 20, если да – вернуть true, в противном случае – false.
-//5. * Написать метод, который определяет является ли год високосным. Каждый 4-й год является
-//    високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
-
-
-
 public class Main_lesson_1 {
 
 
-    public static void Calcultion (double a, double b, double c, double d) {
+    public static float Calculation () {
 
-        double res = a * (b + (c / d));
-        System.out.println("Сумма ...." + res);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите число a = ");
+        float a = sc.nextFloat();
+        System.out.println("Введите число b = ");
+        float b = sc.nextFloat();
+        System.out.println("Введите число c = ");
+        float c = sc.nextFloat();
+        System.out.println("Введите число d = ");
+        float d = sc.nextFloat();
+
+        System.out.print("Сумма = ");
+
+        return a * (b + (c / d));
+    }
+
+    private static boolean inRange (int x,int y){
+
+        int sum = x + y;
+        return sum < 20 && sum > 10;
+
+    }
+
+    public static boolean leapYear(int year) {
+
+        return (year % 100 != 0) && (year % 4 == 0) || (year % 400 == 0);
+
     }
 
     public static void main (String[] args) {
@@ -28,8 +45,8 @@ public class Main_lesson_1 {
         byte bt = 110;
         short sh = 30000;
         int i = 2198765;
-        float f = 17.3f;
-        double doub = 17.3d;
+        float flt = 17.3f;
+        double dbl = 17.3d;
         boolean bool = true;
         char ch = 'C';
 
@@ -37,20 +54,20 @@ public class Main_lesson_1 {
 
 //3. Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат, где a,b,c,d – входные параметры этого метода.
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Введите число a = ");
-        double a = sc.nextDouble();
-        System.out.println("Введите число b = ");
-        double b = sc.nextDouble();
-        System.out.println("Введите число c = ");
-        double c = sc.nextDouble();
-        System.out.println("Введите число d = ");
-        double d = sc.nextDouble();
-
-        Calcultion(a,b,c,d);
 
 
+        System.out.println(Calculation());
+
+
+
+//4. Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах 10 до 20, если да – вернуть true, в противном случае – false.
+
+        System.out.println(inRange(5,6));
+
+//5. * Написать метод, который определяет является ли год високосным. Каждый 4-й год является
+//    високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+
+        System.out.println(leapYear(2017));
 
 
     }
